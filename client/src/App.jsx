ce30f3f3c8578075545edec0c22316242e9abfc0
Fabricio <fabricionasciment77@gmail.com>
2025-01-31 18:17:14 -0300

    Adicionei uma logica para mudar a cor do h1, baseado na cor do BGColor, porem ela so muda entre branco e preto, baseado se a cor do background esta mais perto do preto ou do branco, fiz isso convertendo hexadecimal para rgb e fazendo um if else, se a cor for maior que 128 ela será clara, se menor será branca, enfim ainda tenho que consertar essa lógica para funcionar quando se é digitado e quando está mechendo no color picker

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,75 +5,99 @@ import ColorPicker from './components/ColorPicker';
 import RandomColorButton from './components/RandomColorButton';
 
 function App() {
-  const [bgColor, setBGColor] = useState('#ffffff');  
+  const [h1Color, setH1Color] = useState('#000000');
+  const [bgColor, setBGColor] = useState('#ffffff');
   const [typedColor, setTypedColor] = useState('');
   const [filteredColor, setFilteredColor] = useState([]);
   const [error, setError] = useState(null);
-  
 
-function HandleColor(e){
-  const value = e.target.value;
-  setTypedColor(value);
 
-  const colorFilter = colorList.filter((colorOBJ) => {
-    return colorOBJ.name.toLowerCase().includes(value.toLowerCase());
-  });
-  
-   console.log(colorFilter)
-   setFilteredColor(colorFilter);
+  useEffect(() => {
+    console.log('backgroundcolor updated');
+    handleH1Color(bgColor);
+  }, [bgColor]);
 
-  
-  const colorFinder = colorList.find((colorOBJ)=> 
-    colorOBJ.name.toLowerCase() === value.toLowerCase());
+  function handleH1Color(bgColorHex) {
+    const hex = bgColorHex.replace('#','');
 
-  if(colorFinder){
-    setBGColor(colorFinder.name);
-    setError(null);
+    const r = parseInt(hex.substring(0, 2), 16);
+    const g = parseInt(hex.substring(2, 4), 16);
+    const b = parseInt(hex.substring(4, 6), 16);
+
+    const brilhoMedio = (r + g + b) / 3;
+
+    if(brilhoMedio < 128){
+      setH1Color("#ffffff");
+      return
+    }
+    else{
+     setH1Color("#000000");
+     return
+    }
   }
-  else{
-    setBGColor('white');
+
+  function HandleColor(e) {
+    const value = e.target.value;
+    setTypedColor(value);
+
+    const colorFilter = colorList.filter((colorOBJ) => {
+      return colorOBJ.name.toLowerCase().includes(value.toLowerCase());
+    });
+
+    console.log(colorFilter)
+    setFilteredColor(colorFilter);
+
+
+    const colorFinder = colorList.find((colorOBJ) =>
+      colorOBJ.name.toLowerCase() === value.toLowerCase());
+
+    if (colorFinder) {
+      setBGColor(colorFinder.name);
+      setError(null);
+    }
+    else {
+      setBGColor('#ffffff');
+    }
   }
-}
 
+  function HandlePickedColor(color) {
+    setBGColor(color);
+  }
 
-function HandlePickedColor(color){
-setBGColor(color);
-}
-
-function handleRandomColor(newRandomColor) {
-  setBGColor(newRandomColor);
-}
+  function handleRandomColor(newRandomColor) {
+    setBGColor(newRandomColor);
+  }
 
   return (
-    <div id='section-ONE' style={{backgroundColor: bgColor,}}>
-      <h1>Hello, I am Fabrício!</h1>
+    <div id='section-ONE' style={{ backgroundColor: bgColor }}>
+      <h1 style={{ color: h1Color }}>Hello, I am Fabrício!</h1>
 
       <div>
         <input
-        className='colorInput'
+          className='colorInput'
           type="text"
           placeholder="Type a color name"
           value={typedColor}
-          onChange={(e)=>{HandleColor(e)}}
+          onChange={(e) => { HandleColor(e) }}
           style={{ padding: '0.5rem' }}
         />
 
         <ColorPicker id="ColorPicker" onColorPick={HandlePickedColor} />
         <RandomColorButton onRandomColor={handleRandomColor} />
 
-        {typedColor && filteredColor.length> 0 && (
-        <ul className="color-dropdown">
-        {filteredColor.map((color)=>(
+        {typedColor && filteredColor.length > 0 && (
+          <ul className="color-dropdown">
+            {filteredColor.map((color) => (
               <li
-              key={color.hex}
-              onClick={()=> {
-                setBGColor(color.name);
-                setTypedColor(color.name);
-                setFilteredColor([]);
-              }}
+                key={color.hex}
+                onClick={() => {
+                  setBGColor(color.name);
+                  setTypedColor(color.name);
+                  setFilteredColor([]);
+                }}
               >{color.name}</li>
             ))}
-        </ul>
+          </ul>
         )}
       </div>
     </div>
